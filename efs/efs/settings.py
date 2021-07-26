7bfa963139bc0c407a95c39cd6d5597d3c22f651
kayaraviprakash <kraviprakash@unomaha.edu>
2021-07-26 17:00:15 +0530

    changing settings file

--- efs/efs/settings.py
+++ efs/efs/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'h(2-*k9hv#rw$i4ukrjgja#&w0wtk%mbp88(5kriwok!*wenf('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
